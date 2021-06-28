194b2b41ba337a55dbee22ab79c4d9511285d763
olof-sky <oloof92@gmail.com>
2021-06-28 12:41:44 +0200

    Created db and connected to db

---
+++ src/main/java/com/skylan/jpa_recipe_assignment/JpaRecipeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.skylan.jpa_recipe_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRecipeAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaRecipeAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skylan/jpa_recipe_assignment/JpaRecipeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skylan.jpa_recipe_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRecipeAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
